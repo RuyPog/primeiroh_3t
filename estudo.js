54f9f85db7500c809df16056e40998431d7b2b5c
RuyzinGameplays2011 <110060905+RuyPog@users.noreply.github.com>
2022-09-27 22:27:17 -0300

    27-09

--- estudo.js
+++ estudo.js
@@ -3,5 +3,5 @@ alert("Oi, eu sou um alerta")
         document.write("Aula de Pens. Comp.");
 
         function meChame(){
-                alert("Bobão 🗿");
+                alert("😐🤘");
             }

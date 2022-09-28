345d711371961f5ebb26e9001d16a3754aa5b34c
RuyzinGameplays2011 <110060905+RuyPog@users.noreply.github.com>
2022-09-28 18:05:38 -0300

    28-09

--- estudo.js
+++ estudo.js
@@ -1,7 +1,11 @@
 alert("Oi, eu sou um alerta")
         console.log("https://www.youtube.com/watch?v=N9777WExvCc");
         document.write("Aula de Pens. Comp.");
-
+        let nome = "BernadinhoGameplays";
+        let numero = "26";
+        let turma = "H";
+        let serie = 1;
         function meChame(){
+                document.write("Eu sou"+nome+"Nr"+numero+"do"+serie+turma);
                 alert("😐🤘");
             }

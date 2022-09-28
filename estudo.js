39b3ab1d87d2227d15e93fac2e2ade7970679e63
RuyzinGameplays2011 <110060905+RuyPog@users.noreply.github.com>
2022-09-28 18:13:03 -0300

    28-09

--- estudo.js
+++ estudo.js
@@ -1,9 +1,11 @@
 alert("Oi, eu sou um alerta")
         console.log("https://www.youtube.com/watch?v=N9777WExvCc");
         document.write("Aula de Pens. Comp.");
+       
         let tabuada = 2;
 
         function meChame(){
                 document.write(tabuada + " X 1 = " + (tabuada*1));
+                <br>
                 document.write(tabuada + " X 2 = " + (tabuada*2));
             }

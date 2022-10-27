58f165ad4387692e482f654d157ad7827c636bc8
RuyzinGameplays2011 <110060905+RuyPog@users.noreply.github.com>
2022-10-26 21:45:54 -0300

    26-10

--- estudo.js
+++ estudo.js
@@ -1,13 +1,3 @@
-      function escreva() {
-                for(var i = 0; 1 < 20; i++){
-                        document.write(tabuada + " x 10 = " + i + (tabuada*i) + "<br>")
-                }
-            } 
-            function quadrado(){
-                for(var i = 2; 1 < 21; i++){
-                        document.write("O quadrado de " + i + " é " + (i*i)+"<br>")
-                }
-            }
             function moeda(atual){
                 return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
             }
@@ -19,24 +9,20 @@
                         alert("O valor deve ser um número.");
                         document.getElementById("val").value = "";
                         document.getElementById("val").focus();
-
                         return
                 } 
                 if(!Number(juros)){
                         alert("O valor de juros deve ser um número.");
                         document.getElementById("ju").value = "";
                         document.getElementById("ju").focus();
-
                         return
                 } 
                 if(!Number(mes)){
                         alert("A quantidade de meses deve ser um número.");
                         document.getElementById("mes").value = "";
                         document.getElementById("mes").focus();
-
                         return
                 } 
-        
                 let resultado = 0
                 let texto = "";
                 for(let i = 1; i <= mes; i++){
@@ -44,7 +30,6 @@
                         valor = resultado; 
                          texto += "Mês " + i + " valor " + moeda(resultado)+ "<br>";
                         //document.write("Mês" + i + " valor: " + moeda(resultado) + "<br>"); 
-
                 }
                 document.getElementById("meses").innerHTML = texto;
                 document.getElementById("resultado").innerHTML = "Total: " + moeda(resultado);
